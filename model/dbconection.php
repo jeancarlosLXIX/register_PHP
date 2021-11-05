dd375923bafd7e2e6513b76e367e8f2461f03ff5
Jean Carlos Farias Nina <jfarias@indrhi.local>
2021-11-05 15:12:08 -0400

    Heroku con

--- model/dbconection.php
+++ model/dbconection.php
@@ -1,6 +1,6 @@
 <?php
 
-    $mysqli = new mysqli ("127.0.0.1", "root", '', 'indrhi');
+    $mysqli = new mysqli ("us-cdbr-east-04.cleardb.com", "bb5e05959a0bb2", '73697945', 'heroku_d66806d4df58b5d');
 
     if ($mysqli->connect_errno) {
         throw new RuntimeException('Error: ' . $mysqli->connect_error);
