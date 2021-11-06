4613ff65a0db65d4b99ee3005d7db57edde4df71
Jean <jeancarlossenpai@gmail.com>
2021-11-05 20:59:01 -0400

    stack help

--- view/add.php
+++ view/add.php
@@ -1,4 +1,5 @@
 <?php
+ini_set("display_errors",true);
   require_once __DIR__."/../model/autoloder.inc.php";
   require_once __DIR__."/../model/session.inc.php";
   $_SESSION['title'] = "Agregar equipo";
