f4848f084e7d8cacc29039b3328e36e01295c9dd
Jean <jeancarlossenpai@gmail.com>
2021-11-05 20:22:32 -0400

    path

--- model/query.class.php
+++ model/query.class.php
@@ -75,7 +75,7 @@ class Query {
 		$_SESSION['name'] = $row['name'];
 		$_SESSION['lastname'] = $row['lastname'];
 		$_SESSION['user'] = $row['user'];
-		header("Location: ../view/index.php");
+		header("Location: ./../view/index.php");
     
     }
 }
